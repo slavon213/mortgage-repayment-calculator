c391e97c057f7d585acf95682aad9b25645e2818
slavon213 <slavon213@gmail.com>
2025-08-06 12:36:29 +0300

    add function show empty results

--- script.js
+++ script.js
@@ -5,18 +5,27 @@ const termInput = document.getElementById("term");
 const rateInput = document.getElementById("ratee");
 const buttonsMortgageType = document.querySelectorAll("input[type='radio']");
 const buttonCalculate = document.getElementById("calculate");
+const displayBlock = document.querySelector(".half.second");
+
+const emptyResult = () => {
+    const emptyResultDiv = document.createElement("div");
+    emptyResultDiv.classList.add("pending");
+    emptyResultDiv.classList.add("grd");
+    emptyResultDiv.innerHTML = `
+    <img src="./assets/images/illustration-empty.svg" alt="">
+                        <h2 class="text-preset-2">Results shown here</h2>
+                        <p class="text-preset-4">
+                            Complete the form and click “calculate repayments” to see what your monthly repayments would
+                            be.
+                        </p>
+    `;
+    displayBlock.style.placeContent = "center";
+    displayBlock.appendChild(emptyResultDiv);
+};
 
 
+emptyResult();
 buttonClear.addEventListener("click", clearForm);
-
-
-
-
-
-
-
-
-
 
 function clearForm() {
     formCalculator.reset();
