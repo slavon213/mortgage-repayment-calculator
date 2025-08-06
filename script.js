f1fa338f5fcdc39ec69124137dd253fb778b2d1d
slavon213 <slavon213@gmail.com>
2025-08-06 11:59:53 +0300

    add const  for basic elements

---
+++ script.js
@@ -0,0 +1,10 @@
+const buttonClear = document.getElementById("clearAll");
+const amountInput = document.getElementById("amount");
+const termInput = document.getElementById("term");
+const rateInput = document.getElementById("ratee");
+const buttonsMortgageType = document.querySelectorAll("input[type='radio']");
+const buttonCalculate = document.getElementById("calculate");
+
+
+
+
